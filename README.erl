b8aa928f943f2725428efafa240bfae2102f95c5
Johannes Christ <jc@jchri.st>
2021-07-17 00:53:43 +0200

    Rewrite in Erlang.
    It doesn't exist until it's written in Erlang.

---
+++ README.erl
@@ -0,0 +1,13 @@
+%% @doc Hi there, I'm Joe! 👋
+%%
+%% I'm Joe, a Computer Science student working primarily with Python, Rust and Elixir!
+%% 
+%% I spend most of my time working as an Owner at [Python Discord](https://pythondiscord.com/), focusing mainly on our infrastructure and technology.
+%% 
+%% I'm starting Computer Science at University level this September!
+%%
+%% Check out some of my projects below and be sure to hit me up on Discord at `@joe#6000` or email `joe@jb3.dev`.
+%% 
+%% <img align="left" src="https://github-readme-stats.vercel.app/api?username=jb3&count_private=true&line_height=21&show_icons=true&hide_border=true&theme=dracula"/>
+%% <img align="left" src="https://github-readme-stats.vercel.app/api/top-langs/?username=jb3&layout=compact&card_width=250&hide_border=true&theme=dracula"/>
+%% @end
